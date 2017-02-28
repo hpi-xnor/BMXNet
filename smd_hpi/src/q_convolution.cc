d45f04a7e7f32dfff4f3ce73f43b2c062646b3d6
Martin Fritzsche <martin.fritzsche@citrix.com>
2017-02-28 19:38:20 +0100

    add calculation of alpha and beta values (not applied to result yet)

--- smd_hpi/src/q_convolution.cc
+++ smd_hpi/src/q_convolution.cc
@@ -24,6 +24,91 @@ using ns = std::chrono::nanoseconds;
 using get_time = std::chrono::steady_clock ;
 
 namespace mshadow {
+	inline float get_alpha(float* weight, int width, int height, int depth) {
+		float accum = 0.0f;
+		for (int z = 0; z < depth; ++z) {
+			for (int x = 0; x < width; ++x) {
+				for (int y = 0; y < height; ++y) {
+					accum += weight[z * (width * height) + x * height + y]; //@todo: abs?
+				}
+			}
+		}
+		return accum / (float) (width * height * depth);
+	}
+
+	inline void get_alpha_plane(float* alpha_plane_out, float* weights,
+															int num_weights,
+															int kernel_width, int kernel_height,
+															int input_depth) {
+		for (int i = 0; i < num_weights; i++) {
+			alpha_plane_out[i] = get_alpha(&weights[i * kernel_height * kernel_width * input_depth], kernel_height, kernel_width, input_depth);
+		}
+	}
+
+	inline void get_A_planes(float* A_planes_out, float* input,
+													 int input_depth, int input_width, int input_height,
+													 int batch_size) {
+		for (int i = 0; i < batch_size; i++) {
+			for (int x = 0; x < input_width; ++x) {
+				for (int y = 0; y < input_height; ++y) {
+					float accum = 0.0f;
+					for (int z = 0; z < input_depth; ++z) {
+						accum += input[i * (input_depth * input_width * input_height) +
+													 z * (input_width * input_height) +
+													 x * input_height +
+													 y]; //@todo: abs?
+					}
+					A_planes_out[i * input_width * input_height +
+										  x * input_height +
+											y] = accum / (float) input_depth;
+				}
+			}
+		}
+	}
+
+	inline void get_K_planes(float* K_planes_out, float* A_planes,
+													 int input_width, int input_height,
+													 int kernel_width, int kernel_height,
+													 int batch_size) {
+		int K_width = (input_width - kernel_width + 2 * 0/*padding*/) / 1/*stride*/ + 1;
+		int K_height = (input_height - kernel_height + 2 * 0/*padding*/) / 1/*stride*/ + 1;
+
+		//@todo: super naive conv
+		for (int i = 0; i < batch_size; i ++) {
+			// for every batch
+			for (int kx = 0; kx < K_width; kx++) {
+				for (int ky = 0; ky < K_height; ky++) {
+					// for every kx, ky in our output plane
+					int accum = 0;
+					// we do collect the sum of all values covered by the kernel
+					for (int ix = kx; ix < kx + kernel_width; ix++) {
+						for (int iy = ky; iy < ky + kernel_height; iy++) {
+							accum += A_planes[i * input_width * input_height +
+																ix * input_height +
+																iy];
+						}
+					}
+					// and multiply them with 1/(w * h)
+					K_planes_out[i * K_width * K_height +
+											 kx * K_height +
+											 ky] = accum / ((float) kernel_height * kernel_width);
+				}
+			}
+		}
+	}
+
+	inline void pointwise_mul_mm(float *output, const float *input, int size){
+		for (int i = 0; i < size; i++) {
+			output[i] *= input[i];
+		}
+	}
+
+	inline void pointwise_mul_scalar(float *output, const float scalar, int size){
+		for (int i = 0; i < size; i++) {
+			output[i] *= scalar;
+		}
+	}
+
 	inline BINARY_WORD concatenate(float* array)
 	{
 		BINARY_WORD rvalue=0;
@@ -114,16 +199,34 @@ namespace mshadow {
 
       CHECK_EQ(param.stride[0], 1) << "binary convolution currently only supported with stride==1";
       CHECK_EQ(param.stride[1], 1) << "binary convolution currently only supported with stride==1";
+			CHECK_EQ(param.pad[0], 0) << "cant create beta scaling factor with padded input yet";
+			CHECK_EQ(param.pad[1], 0) << "cant create beta scaling factor with padded input yet";
 
-      ///*
+			///*
       int m = wmat.size(0);
       int n = wmat.size(1);
       int k = in_col.size(1);
+			int batch_size = data.size(0);
+			int input_width = data.size(2);
+			int input_height = data.size(3);
+			int input_depth = data.size(1);
+			int output_width = (input_width - param.kernel[0] + 2 * 0/*padding*/) / 1/*stride*/ + 1;
+			int output_height = (input_height - param.kernel[1] + 2 * 0/*padding*/) / 1/*stride*/ + 1;
       BINARY_WORD* binary_row = (BINARY_WORD*) malloc(m * n/BITS_PER_BINARY_WORD * sizeof(BINARY_WORD));
       BINARY_WORD* binary_col = (BINARY_WORD*) malloc(n * k/BITS_PER_BINARY_WORD * sizeof(BINARY_WORD));
+			float* alpha_plane = (float *) malloc (param.num_filter * sizeof(float));
+			float* A_planes = (float *) malloc (input_width * input_height * batch_size * sizeof(float));
+			float* K_planes = (float *) malloc (output_width * output_height * batch_size * sizeof(float));
 
 			get_binary_row(wmat.dptr_, binary_row, m*n);
 			get_binary_col(in_col.dptr_, binary_col, n, k);
+
+			// alpha
+			get_alpha_plane(alpha_plane, wmat.dptr_, param.num_filter, param.kernel[0], param.kernel[1], input_depth);
+
+			// beta
+			get_A_planes(A_planes, data.dptr_, input_depth, input_width, input_height, batch_size);
+			get_K_planes(K_planes, A_planes, input_width, input_height, param.kernel[0], param.kernel[1], batch_size);
 
 			auto start = std::chrono::high_resolution_clock::now();
 			///*
@@ -143,8 +246,12 @@ namespace mshadow {
 			auto finish = std::chrono::high_resolution_clock::now();
 			std::chrono::duration<double> elapsed = finish - start;
 			std::cout << "xnor Elapsed time: " << elapsed.count() << " s\n";
+
 			free(binary_row);
 			free(binary_col);
+			free(alpha_plane);
+			free(A_planes);
+			free(K_planes);
     }
 
     template<typename DType>
