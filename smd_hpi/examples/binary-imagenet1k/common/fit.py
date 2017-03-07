d1b63576a98d913de10c2b26f8d472351687bb99
haojin.yang <semamediadata@gmail.com>
2017-03-07 11:59:49 +0100

    adapted imagenet1k example

--- smd_hpi/examples/binary-imagenet1k/common/fit.py
+++ smd_hpi/examples/binary-imagenet1k/common/fit.py
@@ -119,8 +119,6 @@ def fit(args, network, data_loader, **kwargs):
     # load model
     if 'arg_params' in kwargs and 'aux_params' in kwargs:
         logging.info('Training model initialized from trained model')        
-        print kwargs['arg_params']
-        print kwargs['aux_params']
         arg_params = kwargs['arg_params']
         aux_params = kwargs['aux_params']
     else:
@@ -128,6 +126,8 @@ def fit(args, network, data_loader, **kwargs):
         if sym is not None:
             assert sym.tojson() == network.tojson()
 
+    fixed_param_names = [name for name in network.list_arguments() \
+        if name.startswith('conv')]
     # save model
     checkpoint = _save_model(args, kv.rank)
 
@@ -141,13 +141,14 @@ def fit(args, network, data_loader, **kwargs):
     # create model
     model = mx.mod.Module(
         context       = devs,
-        symbol        = network
+        symbol        = network,
+        fixed_param_names = fixed_param_names
     )
 
     lr_scheduler  = lr_scheduler
     optimizer_params = {
             'learning_rate': lr,
-            #'momentum' : args.mom,
+            'momentum' : args.mom,
             'wd' : args.wd,
             'lr_scheduler': lr_scheduler}
 
