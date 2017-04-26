85ced1592fd5f16b016d52564f50dc14c233284d
Martin Fritzsche <martin.fritzsche@student.hpi.uni-potsdam.de>
2017-04-26 18:32:45 +0200

    add benchmark code to test several gemm

--- smd_hpi/src/xnor_cpu.cc
+++ smd_hpi/src/xnor_cpu.cc
@@ -6,21 +6,24 @@
 */
 
 #include "xnor_cpu.h"
+#include <chrono>
 
 namespace mxnet {
 namespace op {
 namespace xnor_cpu {
 
-#define UNROLLN	6
+#define UNROLLN 6
 
-void xnor_gemm(int M, int N, int K,
+
+
+void xnor_gemm_optimized(int M, int N, int K,
                         BINARY_WORD *A, int lda,
                         BINARY_WORD *B, int ldb,
                         float *C, int ldc){
     int m,k,n;
     #pragma omp parallel for    
     for (m = 0; m < M; ++m) {
-    	#pragma omp parallel for
+      #pragma omp parallel for
       for (k = 0; k < ((K / UNROLLN) * UNROLLN); k+=UNROLLN) {
         BINARY_WORD A_PART[UNROLLN];
         A_PART[0] = A[m*lda+k];
@@ -44,60 +47,110 @@ void xnor_gemm(int M, int N, int K,
 
       #pragma omp parallel for 
       for (k=(K / UNROLLN) * UNROLLN; k < K; ++k) {
-    		BINARY_WORD A_PART = A[m*lda+k];
-	  		#pragma omp parallel for
-        for (n = 0; n < N; ++n) {
-        	C[m * ldc + n] += __builtin_popcountl(~(A_PART ^ B[k * ldb + n]));
-        }
-      }
-    }
-
-
-  }
-
-
-  // write popc in int array, in the end convert back (only very small perf. improvement)
-  void xnor_gemm_convert_to_int(int M, int N, int K,
-                        BINARY_WORD *A, int lda,
-                        BINARY_WORD *B, int ldb,
-                        float *C, int ldc){
-    int m,k,n;
-    int popc[M*N];
-    #pragma omp parallel for collapse(2)    
-    for (m = 0; m < M; ++m) {
-      for (k = 0; k < K; k++) {
         BINARY_WORD A_PART = A[m*lda+k];
         #pragma omp parallel for
         for (n = 0; n < N; ++n) {
-          popc[m*ldc+n] += __builtin_popcountl(~(A_PART ^ B[k*ldb+n]));
+          C[m * ldc + n] += __builtin_popcountl(~(A_PART ^ B[k * ldb + n]));
         }
       }
     }
 
-    for (int i=0; i < M*N; i++) {
-    	C[i] = popc[i];
-    }
+
   }
 
 
-  // our baseline
-  void xnor_gemm_baseline(int M, int N, int K,
+// write popc in int array, in the end convert back
+void xnor_gemm_convert_to_int(int M, int N, int K,
+                      BINARY_WORD *A, int lda,
+                      BINARY_WORD *B, int ldb,
+                      float *C, int ldc){
+  int m,k,n;
+  int popc[M*N];
+  #pragma omp parallel for collapse(2)    
+  for (m = 0; m < M; ++m) {
+    for (k = 0; k < K; k++) {
+      BINARY_WORD A_PART = A[m*lda+k];
+      #pragma omp parallel for
+      for (n = 0; n < N; ++n) {
+        popc[m*ldc+n] += __builtin_popcountl(~(A_PART ^ B[k*ldb+n]));
+      }
+    }
+  }
+
+  for (int i=0; i < M*N; i++) {
+    C[i] = popc[i];
+  }
+}
+
+
+// our baseline
+void xnor_gemm_baseline(int M, int N, int K,
+                      BINARY_WORD *A, int lda,
+                      BINARY_WORD *B, int ldb,
+                      float *C, int ldc){
+  int m,k,n;
+  #pragma omp parallel for collapse(2)    
+  for (m = 0; m < M; ++m) {
+    for (k = 0; k < K; k++) {
+      BINARY_WORD A_PART = A[m*lda+k];
+      #pragma omp parallel for
+      for (n = 0; n < N; ++n) {
+        C[m*ldc+n] += __builtin_popcountl(~(A_PART ^ B[k*ldb+n]));
+      }
+    }
+  }
+}
+
+// our baseline sans omp
+void xnor_gemm_baseline_no_omp(int M, int N, int K,
+                      BINARY_WORD *A, int lda,
+                      BINARY_WORD *B, int ldb,
+                      float *C, int ldc){
+  int m,k,n;
+  // #pragma omp parallel for collapse(2)    
+  for (m = 0; m < M; ++m) {
+    for (k = 0; k < K; k++) {
+      BINARY_WORD A_PART = A[m*lda+k];
+      // #pragma omp parallel for
+      for (n = 0; n < N; ++n) {
+        C[m*ldc+n] += __builtin_popcountl(~(A_PART ^ B[k*ldb+n]));
+      }
+    }
+  }
+}
+
+
+void xnor_gemm(int M, int N, int K,
                         BINARY_WORD *A, int lda,
                         BINARY_WORD *B, int ldb,
                         float *C, int ldc){
-    int m,k,n;
-    #pragma omp parallel for collapse(2)    
-    for (m = 0; m < M; ++m) {
-      for (k = 0; k < K; k++) {
-        BINARY_WORD A_PART = A[m*lda+k];
-        #pragma omp parallel for
-        for (n = 0; n < N; ++n) {
-          C[m*ldc+n] += __builtin_popcountl(~(A_PART ^ B[k*ldb+n]));
-        }
-      }
+
+//typedef std::map<std::string, int> Map;
+  std::vector<std::pair<std::string, std::function<void(int,int,int,BINARY_WORD*,int,BINARY_WORD*,int,float*,int)>>> gemm_methods;
+  gemm_methods.push_back(std::make_pair("xnor_gemm_baseline", xnor_gemm_baseline));
+  gemm_methods.push_back(std::make_pair("xnor_gemm_baseline (no omp)", xnor_gemm_baseline_no_omp));
+  gemm_methods.push_back(std::make_pair("xnor_gemm_convert_to_int", xnor_gemm_convert_to_int));
+  gemm_methods.push_back(std::make_pair("xnor_gemm_optimized", xnor_gemm_optimized));
+   //gemm_methods.insert(std::make_pair("abc", xnor_gemm_optimized));  
+
+  std::cout << "xnor_gemm with M: " << M << " N: " << N << " K: " << K << std::endl;
+
+  for (auto tuple : gemm_methods) {
+    //reset output array
+    for (int i = 0; i < M * N; ++i) {
+      C[i] = 0;
     }
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    tuple.second(M, N, K, A, lda, B, ldb, C, ldc);
+
+    auto finish = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = finish - start;
+    std::cout << elapsed.count() << "s (" << tuple.first << ")" << std::endl; 
   }
 
+}
 
 } //namespace xnor_cpu
 } //namespace op
