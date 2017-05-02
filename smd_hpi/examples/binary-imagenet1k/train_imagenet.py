b2dd2083417589021f45aa6cf31851e905265798
haojin.yang <haojin.yang@hpi.de>
2017-05-02 14:15:21 +0200

    adapted resnet config

--- smd_hpi/examples/binary-imagenet1k/train_imagenet.py
+++ smd_hpi/examples/binary-imagenet1k/train_imagenet.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
     args_params=None
     auxs_params=None
     if args.pretrained:
-        new_sym, args_params, auxs_params = mx.model.load_checkpoint(args.pretrained, 50)
+        new_sym, args_params, auxs_params = mx.model.load_checkpoint(args.pretrained, 37)
         logger.info("Start training with {} from pretrained model {}"
                 .format(str(devs), args.pretrained))
 
