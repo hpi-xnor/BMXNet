5d6431f75afce3fbd6c0c500bd10cb729f7a7031
haojin.yang <haojin.yang@hpi.de>
2017-10-11 13:46:28 +0200

    fallback to q_conv_v1, re-trained model 'binarized_imagenet-resnet18-64bit-1st-stage-fullprecision'

--- smd_hpi/examples/binary-imagenet1k/train_imagenet.py
+++ smd_hpi/examples/binary-imagenet1k/train_imagenet.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     parser.set_defaults(
         # network
         network        = 'resnet',
-        num_layers     = 50,
+        num_layers     = 18,
 
         # data
         num_classes      = 1000,
@@ -34,12 +34,12 @@ if __name__ == '__main__':
         min_random_scale = 1, # if input image has min size k, suggest to use
                               # 256.0/x, e.g. 0.533 for 480
         # train
-        num_epochs       = 5,
-        lr_step_epochs   = '1,2,3,4',
-        lr               = 0.001,
-	lr_factor        = 0.1,
+        num_epochs       = 40,
+        lr_step_epochs   = '5,10,15,20,25,30,35',
+        lr               = 0.0001,
+	lr_factor        = 0.25,
         batch_size     = 32,
-        optimizer        = 'sgd',
+        optimizer        = 'adam',
         disp_batches     = 10,
         top_k            = 5,
         data_train       = '/data/imagenet1k/imagenet1k-train.rec',
