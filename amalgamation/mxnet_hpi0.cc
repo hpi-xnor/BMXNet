79220cc11679454f77f3feaccab07c58fff34185
haojin.yang <semamediadata@gmail.com>
2017-10-19 16:45:48 +0200

    adapted makefile/config for amalgmation

--- amalgamation/mxnet_hpi0.cc
+++ amalgamation/mxnet_hpi0.cc
@@ -1,6 +1,7 @@
 // hpi
 
 #include "smd_hpi/src/q_convolution.cc"
+#include "smd_hpi/src/q_convolution_v1.cc"
 #include "smd_hpi/src/q_activation.cc"
 #include "smd_hpi/src/q_fully_connected.cc"
 #include "smd_hpi/src/xnor_cpu.cc"
